43325fe40f0968c7eba924bd501582c3288e2d67
Artem <kazuta.artem@gmail.com>
2022-08-28 23:03:09 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -6,10 +6,10 @@ let header = document.querySelector(".main-header");
 
 header.classList.toggle("main-header--bottom");
 menu.classList.add("navigation__main-list--close");
-buttonOpen.classList.toggle("burger--unshow");
+buttonOpen.classList.toggle("burger__toggle--unshow");
 
 button.addEventListener("click", function() {
-  buttonOpen.classList.toggle("burger--unshow");
-  buttonClose.classList.toggle("burger--unshow");
+  buttonOpen.classList.toggle("burger__toggle--unshow");
+  buttonClose.classList.toggle("burger__close--unshow");
   menu.classList.toggle("navigation__main-list--close");
 });
